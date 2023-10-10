2795dcb80f7e08f66034fe445f46122f008eecfa
Leo-MouraF <leom.frigotto@gmail.com>
2023-10-10 10:51:36 -0300

    Nem sei mais o que eu to fazendo

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, abort, request, redirect, url_for
+from uuid import uuid4
+
+from data.produtos import produtos
 
 app = Flask(__name__)
 
@@ -16,3 +21,53 @@ def suplementos():
 @app.route("/produtos_naturais")
 def produtos_naturais():
     return render_template("produtos_naturais.html")
+
+@app.route("/produto/<produto_id>")
+def single_produto(produto_id):
+    produto = produtos.get(produto_id)
+    if (produto == None):
+        abort(404)
+    else:
+        return render_template("single_produto.html", produto=produto)
+
+@app.route('/submit_item', methods=['GET', 'POST'])
+def submit_item():
+    if request.method == 'POST':
+        novo_item = gerar_novo_produto()
+        id = novo_item['id']
+        print(novo_item)
+        print(produtos)
+        return redirect(url_for('single_produto', produto_id=id))
+    else:
+        return  render_template('submit_item.html')
+
+
+def gerar_novo_produto():
+    name = str(request.form.get('nome'))
+    preco = request.form.get('valor')
+    description = str(request.form.get('descricao'))
+    category = str(request.form.get('categoria'))
+    novo_produto = {
+        'id':str(uuid4()),
+        'nome':name,
+        'preco':preco,
+        'descricao':description,
+        'categoria':category
+    }
+    
+    produtos_json = ler_o_json()
+    escrever_no_json(produtos_json, novo_produto)
+
+    return novo_produto
+
+def escrever_no_json(produtos, novo_produto):
+    with open('data/produtos.json', 'a') as arquivo_produtos:
+        produtos[novo_produto] = novo_produto
+        novo_produto_json = json.dump(produtos, indent=4)
+        arquivo_produtos.writelines(novo_produto_json)
+
+def ler_o_json():
+    with open('data/produtos.json', 'r') as arquivo_produtos:
+        produtos_dicionario = json.load(arquivo_produtos)
+
+        return produtos_dicionario
---
+++ data/produtos.py
@@ -0,0 +1,31 @@
+produtos = {
+    'produto_1':{
+        'id':'produto_1',
+        'nome':'Proteína',
+        'preco':99.99,
+        'descricao':'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+        'url':'img-2.jpg',
+        'categoria':'suplemento'
+    },
+    'produto_2':{
+        'nome':'Proteína',
+        'preco':99.99,
+        'descricao':'lorem ipsum',
+        'url':'img3.jpg',
+        'categoria':'suplemento'
+    },
+    'produto_3':{
+        'nome':'Produto Natural',
+        'preco':99.99,
+        'descricao':'lorem ipsum',
+        'url':'produtonatural2.jpg',
+        'categoria':'produto_natural'
+    },
+    'produto_4':{
+        'nome':'Produto Natural',
+        'preco':99.99,
+        'descricao':'lorem ipsum',
+        'url':'produtonatural1.jpg',
+        'categoria':'produto_natural'
+    },
+}
